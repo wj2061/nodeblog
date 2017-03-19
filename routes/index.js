9aad2f95f3b9e1c923a038a8bb4a028da1564dc2
wj2061 <wj2061@126.com>
2017-03-19 23:50:41 +0800

    finish 4.7

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ module.exports = function (app) {
 	});
 	app.use('/signup', require('./signup'));
 	app.use('/signin', require('./signin'));
-	app.use('./signout', require('./signout'));
-	app.use('./posts', require('./posts'));
+	app.use('/signout', require('./signout'));
+	app.use('/posts', require('./posts'));
 };
